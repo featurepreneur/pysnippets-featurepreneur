2934a576eea98ec7e584db9877aae0254fed4a8e
Raja CSP Raman <raja.csp@gmail.com>
2020-11-09 00:35:46 -0500

    Watermark applied

--- lil_boys_work/bdb_watermark.py
+++ lil_boys_work/bdb_watermark.py
@@ -19,6 +19,14 @@ Source:
     https://www.1001freefonts.com/decorative-fonts.php
 
     https://drive.google.com/drive/folders/1eHDFJIGGGCtNZXpIthBf6hp97CqhcZVM
+
+    /Users/rajacsp/datasets/fonts/Open_Sans/
+
+    https://fonts.google.com/
+
+    https://stackoverflow.com/questions/43708681/pil-png-image-as-watermark-for-a-jpg-image
+
+    https://stackoverflow.com/questions/54366843/how-to-resize-png-image-in-opencv-python
 '''
 
 # Import necessary modules 
@@ -26,12 +34,35 @@ from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 
-FOLDER_LOCAITON = '/Users/rajacsp/datasets/'
+FOLDER_LOCAITON = '/Users/rajacsp/d/tact/The Big Data Boss/BDB-Memes/Lil-Boys/'
 
-FILENAME        = 'BDB_Meme_1014.jpg'
-OUTPUT_FILENAME = 'BDB_Meme_1014_watermark.jpg'
+FILENAME        = 'BDB_Meme_1002.jpg'
+OUTPUT_FILENAME = 'test_watermark.jpg'
 
-def apply_watermark(input_path, output_path, watermark_text):
+FONT_LOCAITON = '/Users/rajacsp/datasets/fonts/Open_Sans/OpenSans-Light.ttf'
+
+WATERMARK_LOCATION = '/Users/rajacsp/d/tact/The Big Data Boss/logo/Big_Data_Boss_Final.png'
+
+RESIZE_FACTOR = 0.02
+
+def apply_watermark_image(input_path, output_path):
+
+    photo = Image.open(input_path)
+    watermark = Image.open(WATERMARK_LOCATION)
+
+    ph_width, ph_height = photo.size
+    wm_width, wm_height = watermark.size
+
+    wm_witdh_new = int(wm_width * RESIZE_FACTOR)
+    wm_height_new = int(wm_height * RESIZE_FACTOR)
+
+    watermark = watermark.resize((wm_witdh_new, wm_height_new))
+    
+    photo.paste(watermark, ((ph_width - wm_height_new - 150), (ph_height - wm_height_new - 70)), watermark)
+
+    photo.save(output_path)
+
+def apply_watermark_text(input_path, output_path, watermark_text):
 
     photo = Image.open(input_path)
 
@@ -43,7 +74,7 @@ def apply_watermark(input_path, output_path, watermark_text):
 
     # make the image editable
     drawing = ImageDraw.Draw(photo)
-    font = ImageFont.truetype("/Users/rajacsp/datasets/fonts/Drift.ttf", 68)
+    font = ImageFont.truetype(FONT_LOCAITON, 50)
 
     #get text width and height
     text_w, text_h = drawing.textsize(watermark_text, font)
@@ -64,7 +95,9 @@ def startpy():
     input_path = FOLDER_LOCAITON + FILENAME
     output_path = FOLDER_LOCAITON + OUTPUT_FILENAME
     
-    apply_watermark(input_path, output_path, '@bigdataboss/')
+    # apply_watermark_text(input_path, output_path, '@bigdataboss/')
+
+    apply_watermark_image(input_path, output_path)
 
 
 if __name__ == '__main__':
