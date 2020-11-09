995b898c23fd7cfd375e339cc537f1646ab364f9
Raja CSP Raman <raja.csp@gmail.com>
2020-11-08 22:48:09 -0500

    Lil boys work added

---
+++ lil_boys_work/actress_work.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# the above line is to avoid 'SyntaxError: Non-UTF-8 code starting with' error
+
+'''
+Created on 
+
+Course work: 
+
+@author: raja
+
+Source:
+    
+'''
+
+# Import necessary modules 
+
+
+def startpy():
+    
+    content =  "Reena"
+
+    print(content)
+
+
+if __name__ == '__main__':
+    startpy()
